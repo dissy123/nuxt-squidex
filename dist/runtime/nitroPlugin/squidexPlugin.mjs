9fed9650fe509502bd92b3b42851f2ee65810cfd
dissy123 <hi@matthiasdollfuss.dev>
2022-12-12 11:52:00 +0100

    check if config now works

--- dist/runtime/nitroPlugin/squidexPlugin.mjs
+++ dist/runtime/nitroPlugin/squidexPlugin.mjs
@@ -1,17 +1,3 @@
-import { SquidexClient } from "../squidex/SquidexClient.mjs";
 import { defineNitroPlugin } from "nitropack/dist/runtime/plugin";
 export default defineNitroPlugin((nitroApp) => {
-  console.log("Add Squidex Nitro Plugin");
-  console.log(process.env.RUNTIME_CONFIG);
-  const config = process.env.RUNTIME_CONFIG["public"].squidex;
-  const client = new SquidexClient(
-    config.id,
-    config.secret,
-    config.appName,
-    config.baseUrl,
-    config.defaultLanguage,
-    config.flatten,
-    config.debug
-  );
-  nitroApp.squidex = client;
 });
--- src/runtime/nitroPlugin/squidexPlugin.ts
+++ src/runtime/nitroPlugin/squidexPlugin.ts
@@ -2,9 +2,13 @@ import { SquidexClient } from "../squidex/SquidexClient";
 import { defineNitroPlugin } from "nitropack/dist/runtime/plugin";
 
 export default defineNitroPlugin((nitroApp) => {
-  console.log("Add Squidex Nitro Plugin");
+  /*console.log("Add Squidex Nitro Plugin");
 
-  console.log(process.env.RUNTIME_CONFIG);
+  console.dir(nitroApp);
+
+  console.dir(process.env.RUNTIME_CONFIG["public"]["squidex"]);
+
+  //const config = useRuntimeConfig().public.squidex;
 
   const config = process.env.RUNTIME_CONFIG["public"].squidex;
 
@@ -17,5 +21,5 @@ export default defineNitroPlugin((nitroApp) => {
     config.flatten,
     config.debug
   );
-  nitroApp.squidex = client;
+  nitroApp.squidex = client;*/
 });
